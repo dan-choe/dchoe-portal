dd405d2dddba665668f973506551a0ec85655a8b
Dan Choe <devdanchoe@gmail.com>
2018-06-15 16:10:26 -0400

    Update UserController.java

--- src/main/java/com/dchoe/portal/controller/UserController.java
+++ src/main/java/com/dchoe/portal/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.dchoe.portal.controller;
 
 import com.dchoe.portal.model.User;
+import com.dchoe.portal.repository.portalRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,11 +17,34 @@ import java.util.List;
 @CrossOrigin("*")
 public class UserController
 {
+    @Autowired
+    portalRepository portalRepository;
+    
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     public ResponseEntity<List<User>> getUsers()
     {
-        List<User> userList = new ArrayList<User>();
-        userList.add(new User(1,"test","test-last","test@gmail.com"));
-        return new ResponseEntity<List<User>>(userList,HttpStatus.OK);
+        //List<User> userList = new ArrayList<User>();
+        //userList.add(new User(1,"test","test-last","test@gmail.com"));
+        //return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
+        return portalRepository.findAll();
     }
+    
+    @GetMapping("/users")
+    public ReponseEntity User getUser(@RequestParam String target)
+    {
+        return portalRepository.findOne(target);
+    }
+    
+    @DeleteMapping("/users")
+    public ReponseEntity User removeUser(@RequestParam(value = "userid", required = true) Integer userID)
+    {
+        portalRepository.findById(id)
+            .map(x -> {
+                portalRepository.deleteById(id);
+                return ResponseEntity.ok().build();
+            })
+            .orElse(ResponseEntity.notFound().build());
+        // return new ResponseEntity<String>("DELETE Response", HttpStatus.OK);
+    }
+       
 }
