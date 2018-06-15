f4ac81c4bc3d477428f6a45575cd54bbed53c828
Dan Choe <devdanchoe@gmail.com>
2018-06-15 16:14:27 -0400

    Update portalRepository.java

--- src/main/java/com/dchoe/portal/repositories/portalRepository.java
+++ src/main/java/com/dchoe/portal/repositories/portalRepository.java
@@ -4,7 +4,8 @@ import com.dchoe.portal.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+// <type of values, type of id>
 @Repository
-public interface portalRepository extends MongoRepository<User, String> {
+public interface portalRepository extends MongoRepository<User, Integer> {
   List<User> findByName(@Param("username") String username);
 }
