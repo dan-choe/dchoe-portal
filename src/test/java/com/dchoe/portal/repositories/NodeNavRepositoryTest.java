67bee428399561d107c4a6957507dfd24439ede6
dan-choe <devdanchoe@gmail.com>
2018-06-17 05:17:46 -0400

    -

--- src/test/java/com/dchoe/portal/repositories/NodeNavRepositoryTest.java
+++ src/test/java/com/dchoe/portal/repositories/NodeNavRepositoryTest.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-//@ActiveProfiles("Test NodeNav")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureDataMongo
